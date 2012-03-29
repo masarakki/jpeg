531827ac6c5f513ba833a53e51bcfe24868dd9ab
Yamada Masaki <masaki@hisme.net>
2012-03-29 10:01:10 +0900

    add Jpeg#open_buffer

--- lib/jpeg.rb
+++ lib/jpeg.rb
@@ -3,6 +3,8 @@ require 'jpeg.so'
 class Jpeg
   def self.open_buffer(str)
     tmp = Tempfile::new("ruby-jpeg")
+    tmp.puts str
+    tmp.rewind
     jpeg = Jpeg.open(tmp.path)
     tmp.close
     jpeg
