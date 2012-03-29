41b4bff1f375449670771de5e9c3a1c05a7955bb
Yamada Masaki <masaki@hisme.net>
2012-03-29 14:39:08 +0900

    copy as binary

--- lib/jpeg.rb
+++ lib/jpeg.rb
@@ -3,6 +3,7 @@ require 'jpeg.so'
 class Jpeg
   def self.open_buffer(str)
     tmp = Tempfile::new("ruby-jpeg")
+    tmp.set_encoding Encoding::BINARY if tmp.respond_to?(:set_encoding)
     tmp.puts str
     tmp.rewind
     jpeg = Jpeg.open(tmp.path)
